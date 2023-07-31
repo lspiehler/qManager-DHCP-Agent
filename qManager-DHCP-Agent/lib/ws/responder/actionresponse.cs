56932210b340475e5f1bc57d330d35b9ca4b817e
Lyas Spiehler <lspiehler@gmail.com>
2023-07-31 07:35:30 -0500

    increment version

--- qManager-DHCP-Agent/lib/ws/responder/actionresponse.cs
+++ qManager-DHCP-Agent/lib/ws/responder/actionresponse.cs
@@ -83,7 +83,7 @@ namespace qManager_DHCP_Agent.lib.ws.responder
                 {
                     {"hostname", commonlib.GetLocalhostFqdn()},
                     {"agentType", "dhcp" },
-                    {"agentVersion", "0.04" }
+                    {"agentVersion", "0.6" }
                 };
             }
             else
